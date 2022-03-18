de85bb9e9ae84ecb7502d108eafcaa207b9aa4bf
kobravania <www.kobriwana@gmail.com>
2022-03-18 21:13:51 +0300

    models

--- NoName/app_news/models.py
+++ NoName/app_news/models.py
@@ -1,11 +1,16 @@
+from tabnanny import verbose
 from django.db import models
 
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    slag = models.CharField(max_length=20)
-    description = models.TextField()
-    datetime = models.DateTimeField()
-    url_source = models.URLField(max_length=100)
-    image = models.FilePathField(path="/img")
+class News(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название статьи")
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField(verbose_name="Содержание")
+    datetime = models.DateTimeField(verbose_name="Дата публикации")
+    url_source = models.CharField(max_length=200, verbose_name="Источник")
+    image = models.ImageField(verbose_name="Изображение")
+
+    class Meta():
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
 
 
