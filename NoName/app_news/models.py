191895c7d9d0da22db0f99e27eb7a93e3ce7b4b5
kobravania <www.kobriwana@gmail.com>
2022-03-18 20:24:20 +0300

    models

--- NoName/app_news/models.py
+++ NoName/app_news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    slag = models.CharField(max_length=20)
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    url_source = models.URLField(max_length=100)
+    image = models.FilePathField(path="/img")
+
+
