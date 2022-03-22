07144198f12a0ba661122403271594b4c9201cea
kobravania <www.kobriwana@gmail.com>
2022-03-22 20:08:35 +0300

    slug

--- NoName/app_news/urls.py
+++ NoName/app_news/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import about, contact, test_view, category, post, new
+from .views import NewDetailView, about, contact, HomeListView, category, post
 
 urlpatterns = [
-    path('',test_view, name="home"),
-    path('new/', new, name='new'),
+    path('', HomeListView.as_view(), name="home"),
+    path('new/<slug:slug>/', NewDetailView.as_view(), name='new'),
     path('cat/', category, name='category'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
