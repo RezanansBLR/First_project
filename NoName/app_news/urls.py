d7cbc5b333458d2e087fbb4f01ce946071105afd
RezanansBLR <nikita19961032@gmail.com>
2022-03-22 14:21:35 +0300

    Temlates

--- NoName/app_news/urls.py
+++ NoName/app_news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import test_view
+from .views import about, contact, test_view, category, post
 
 urlpatterns = [
-    path('',test_view, name="home")
+    path('',test_view, name="home"),
+    path('cat/', category, name='category'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('single-post/', post, name='post'),
 ]
--- NoName/app_news/views.py
+++ NoName/app_news/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def test_view(request):
     return render(request, 'index.html', context={})
+
+def category(request):
+    return render(request, 'category.html', context={})
+
+def about(request):
+    return render(request, 'about-us.html', context={})
+
+def contact(request):
+    return render(request, 'contact.html', context={})
+    
+def post(request):
+    return render(request, 'single-post.html', context={})
