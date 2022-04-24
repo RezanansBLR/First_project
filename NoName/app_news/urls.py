9d21cd4bfe0fc72980b211c5517b1e88cfa8f5bf
RezanansBLR <nikita19961032@gmail.com>
2022-04-24 21:04:42 +0300

    Rezanans

--- NoName/app_news/urls.py
+++ NoName/app_news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import NewDetailView, about, contact, HomeListView, category, post
+from .views import NewDetailView, NewsCatListView, HomeListView, post, about, contact
 
 urlpatterns = [
     path('', HomeListView.as_view(), name="home"),
     path('new/<slug:slug>/', NewDetailView.as_view(), name='new'),
-    path('cat/', category, name='category'),
+    path('cat/<slug:slug>/', NewsCatListView.as_view(), name='category'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('single-post/', post, name='post'),
